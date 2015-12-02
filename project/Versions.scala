67b2157e5696e5a11fda872003d26312cd28aeae
gustavonalle <gustavonalle@gmail.com>
2015-12-02 20:39:00 +0000

    ISPRK-15 Update to Spark 1.5.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
    val infinispanVersion = "8.1.0.Beta1"
    val protoStreamVersion = "3.0.4.Final"
-   val sparkVersion = "1.4.0"
+   val sparkVersion = "1.5.2"
    val scalaTestVersion = "2.2.5"
    val junitVersion = "4.11"
    val twitter4jVersion = "3.0.3"
