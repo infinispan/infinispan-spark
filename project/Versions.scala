a6bc51804ce0ac7d8455104c0555c24fe4c7e88a
gustavonalle <gustavonalle@gmail.com>
2016-02-25 10:15:29 +0000

    ISPRK-18 Update to Spark 1.6.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
    val infinispanVersion = "8.2.0.Beta2"
    val protoStreamVersion = "3.0.4.Final"
-   val sparkVersion = "1.5.2"
+   val sparkVersion = "1.6.0"
    val scalaTestVersion = "2.2.5"
    val junitVersion = "4.11"
-   val twitter4jVersion = "3.0.3"
+   val twitter4jVersion = "4.0.4"
    val jcipAnnotationsVersion = "1.0"
    val scalaDMRVersion = "0.2.0"
    val shrinkWrapVersion = "1.2.3"
