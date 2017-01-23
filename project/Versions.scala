62d2007db96ade4ec48939f2c79c8f418e87def3
gustavonalle <gustavonalle@gmail.com>
2017-01-23 12:28:52 +0000

    ISPRK-41 Updated to Spark 2.1.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
    val infinispanVersion = "9.0.0.Alpha4"
    val protoStreamVersion = "3.0.5.Final"
-   val sparkVersion = "2.0.0"
+   val sparkVersion = "2.1.0"
    val scalaTestVersion = "2.2.5"
    val junitVersion = "4.11"
    val twitter4jVersion = "4.0.4"
