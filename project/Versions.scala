09d626d4dcd265757026c7ee24b3a2a07d176fa7
gustavonalle <gustavonalle@gmail.com>
2017-03-31 11:48:19 +0100

    Update to Infinispan 9.0.0.Final

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
-   val infinispanVersion = "9.0.0.Beta2"
-   val protoStreamVersion = "4.0.0.Alpha4"
+   val infinispanVersion = "9.0.0.Final"
+   val protoStreamVersion = "4.0.0.CR3"
    val sparkVersion = "2.1.0"
    val scalaTestVersion = "2.2.5"
    val junitVersion = "4.11"
