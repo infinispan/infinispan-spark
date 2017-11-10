448bed8679fa4b3f011e361d3da591f41f730c3e
Gustavo Fernandes <gustavonalle@gmail.com>
2017-11-10 16:01:12 -0200

    ISPRK-50 Updated to Infinispan 9.2.0.Beta1

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
-   val infinispanVersion = "9.2.0.Alpha1"
-   val protoStreamVersion = "4.2.0.Alpha3"
+   val infinispanVersion = "9.2.0.Beta1"
+   val protoStreamVersion = "4.2.0.Alpha4"
    val sparkVersion = "2.2.0"
    val scalaTestVersion = "2.2.5"
    val junitVersion = "4.11"
